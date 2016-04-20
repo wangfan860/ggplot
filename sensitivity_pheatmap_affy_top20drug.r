3fb273d3c88478db4211ea1a2b21be7e5d602156
Fan Wang <fan1@uchicago.edu>
2016-04-20 12:01:57 -0500

    Create sensitivity_pheatmap_affy_top20drug.r

---
+++ sensitivity_pheatmap_affy_top20drug.r
@@ -0,0 +1,12 @@
+library(pheatmap)
+
+a= read.csv("cluster_sensitivity_top20.csv")
+rownames(a)=a[,1]
+b=data.matrix(a[,3:22])
+
+annotation_row = data.frame(GeneClass = factor(rep(c("DIF", "IMR", "MES","PRO"), c(149, 114, 136,121))))
+rownames(annotation_row) = rownames(a)
+
+
+hmcols<- colorRampPalette(c("green","red"))(10)
+pheatmap(b,  annotation_row = annotation_row,cluster_row=FALSE,color=hmcols,  fontsize=9, fontsize_row=6, scale="row")
