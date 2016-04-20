f275afea122d3ee12fa4227a88f37704cfa4aa55
Fan Wang <fan1@uchicago.edu>
2016-04-20 10:16:52 -0500

    add colors

--- GENE_expre_pheatmap.r
+++ GENE_expre_pheatmap.r
@@ -6,5 +6,7 @@ b=data.matrix(a[,3:522])
 
 annotation_row = data.frame(GeneClass = factor(rep(c("DIF", "IMR", "MES","PRO"), c(20, 22, 37,21))))
 rownames(annotation_row) = rownames(a)
-pheatmap(b, annotation_row = annotation_row,cluster_row=FALSE,fontsize=9, fontsize_row=6)
+
  
+hmcols<- colorRampPalette(c("blue","red"))(10)
+pheatmap(b,  annotation_row = annotation_row,cluster_row=FALSE,color=hmcols,  fontsize=9, fontsize_row=6, scale="row")
