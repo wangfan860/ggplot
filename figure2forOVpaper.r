b7192024ba6bfc2d596e358ae65374bf7f5e9bb4
Fan Wang <fan1@uchicago.edu>
2016-07-12 15:02:41 -0500

    change color

--- figure2forOVpaper.r
+++ figure2forOVpaper.r
@@ -7,5 +7,5 @@ write.csv(x.order, "ov-ranking-BIBW-AZD6244-ABT888-gefitinib.csv")
 data=read.csv("ov-ranking-BIBW-AZD6244-ABT888-gefitinib.csv",header=T)
 data_long <- melt(data, id="Pt.ID")
 
-cbbPalette <- c( "#56B4E9",  "#999999")
+cbbPalette <- c( "#56B4E9",  "#E69F00")
 ggplot(data=data_long,aes(x=Pt.ID, y=value, colour=variable)) +geom_point(size = 1) +theme(legend.text = element_text( size = 10, face = "bold"))+theme(legend.position="bottom")+geom_smooth(method = "lm", se = FALSE, size=0.85) +scale_colour_manual(values=cbbPalette)
