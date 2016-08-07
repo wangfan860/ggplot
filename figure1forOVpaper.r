d984ad61ebaf30aa64a2e44faafe8d6ceec9c872
Fan Wang <fan1@uchicago.edu>
2016-08-06 21:58:40 -0500

    add 80% figure

--- figure1forOVpaper.r
+++ figure1forOVpaper.r
@@ -1,3 +1,10 @@
 data=read.csv("ggplot.csv",header=T)
 data_long <- melt(data, id="Pt.ID")
 ggplot(data=data_long,aes(x=Pt.ID, y=value, colour=variable)) +geom_line(size = 0.75) +annotate("rect", xmin = 0, xmax = 257, ymin = -5, ymax = 3.5, alpha = .2)+annotate("rect", xmin = 257, xmax = 521, ymin = -5, ymax = 3.5, alpha = .4)+theme(legend.text = element_text( size = 10, face = "bold"))+theme(legend.position="bottom")
+
+
+####80%figure###
+library(reshape)
+data=read.csv("ggplot.csv",header=T)
+data_long <- melt(data, id="Pt.ID")
+ggplot(data=data_long,aes(x=Pt.ID, y=value, colour=variable)) +geom_line(size = 0.75) +annotate("rect", xmin = 0, xmax = 416, ymin = -5, ymax = 3.5, alpha = .2)+annotate("rect", xmin = 416, xmax = 521, ymin = -5, ymax = 3.5, alpha = .4)+theme(legend.text = element_text( size = 14, face = "bold"))+theme(legend.position="bottom")
